26cdfb9e8b2c10f06f549c46e309ddc8fe04da37
Sandeep <formycore@gmail.com>
2022-11-23 21:55:28 +0530

    added github codespaces

---
+++ src/test/java/dev/samantha/hellocodespaces/HelloCodespacesApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.samantha.hellocodespaces;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloCodespacesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
